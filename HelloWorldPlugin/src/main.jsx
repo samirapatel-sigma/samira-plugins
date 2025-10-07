844f7e7cf344642081d72a7c46b5ee51004d5441
Samira Patel <124626694+samirapatel-sigma@users.noreply.github.com>
2025-10-07 17:46:40 -0400

    Delete HelloWorldPlugin directory

--- HelloWorldPlugin/src/main.jsx
+++
@@ -1,1 +0,0 @@
-"import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport App from './App.jsx';\nimport './index.css';\n\nReactDOM.createRoot(document.getElementById('root')).render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>\n);"
