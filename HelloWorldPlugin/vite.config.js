837317d61f69c383472b536ef0d97b081e3ca3d8
Samira Patel <124626694+samirapatel-sigma@users.noreply.github.com>
2025-10-08 13:37:34 -0400

    Update vite.config.js

--- HelloWorldPlugin/vite.config.js
+++ HelloWorldPlugin/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/${{ github.repository }}/${{ github.event.inputs.folderName }}/',
+  base: '/samira-plugins/HelloWorldPlugin/',
 })
